6fcedfe6166f69d37127d25d5881b25798188066
Ben Cavins <ben.cavins@flatironschool.com>
2024-03-27 13:21:43 -0400

    add Card model

---
+++ backend/src/models.py
@@ -0,0 +1,24 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+
+
+metadata = MetaData(naming_convention={
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+})
+
+db = SQLAlchemy(metadata=metadata)
+
+
+class Card(db.Model):
+    __tablename__ = 'cards'
+
+    id = db.Column(db.Integer, primary_key=True)
+    img_url = db.Column(db.String)
+    answer = db.Column(db.String)
+
+    def __repr__(self) -> str:
+        return f"<Card {self.answer} {self.img_url}>"
