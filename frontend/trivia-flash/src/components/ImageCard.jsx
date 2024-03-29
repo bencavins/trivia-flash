8c3a2fb43fa090aab5e44aa704a81d0d5e0e7fd8
Ben Cavins <ben.cavins@flatironschool.com>
2024-03-29 10:57:58 -0400

    check user response to card

--- frontend/trivia-flash/src/components/ImageCard.jsx
+++ frontend/trivia-flash/src/components/ImageCard.jsx
@@ -1,12 +1,25 @@
 import './ImageCard.css'
+import { useState } from 'react'
 
 export default function ImageCard({ img_url, answer }) {
+  const [msg, setMsg] = useState("")
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    if (event.target['user-answer'].value.toLowerCase() == answer.toLowerCase()) {
+      setMsg('Correct!')
+    } else {
+      setMsg(`Incorrect! (answer: ${answer})`)
+    }
+  }
+
   return (
     <div>
       <img src={img_url} />
-      <form>
+      {msg ? <p>{msg}</p> : null}
+      <form onSubmit={handleSubmit}>
         <label>Name the movie: </label>
-        <input type="text" />
+        <input type="text" name="user-answer" />
         <input type="submit" />
       </form>
     </div>
