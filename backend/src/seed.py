59ecb395086a77b71bd63301c1ffc6eb6a195802
Ben Cavins <ben.cavins@flatironschool.com>
2024-04-01 15:35:45 -0400

    switch to mongodb

--- backend/src/seed.py
+++ backend/src/seed.py
@@ -5,15 +5,33 @@ def load_questions():
     questions = [
         {
             'img_url': 'https://m.media-amazon.com/images/M/MV5BNThlZjI0MTgtODMzOC00ZjI2LWI3MTktMGQ3Y2FlM2Y5MDYwXkEyXkFqcGdeQXVyMTIwMjY0NjQz._V1_.jpg', 
-            'answer': 'Get a Clue'
+            'answer': 'Get a Clue',
+            'category': 'Movies',
+            'subcategory': 'Disney Channel Original Movies',
         },
         {
             'img_url': 'https://m.media-amazon.com/images/M/MV5BOTZlYzU4MzAtZWQzMi00MmI1LWIxMWYtZjYxZmYzZTQ4ZjE2XkEyXkFqcGdeQXVyNTg3Njg4ODI@._V1_.jpg', 
-            'answer': 'Gotta Kick It Up!'
+            'answer': 'Gotta Kick It Up!',
+            'category': 'Movies',
+            'subcategory': 'Disney Channel Original Movies',
         },
         {
             'img_url': 'https://m.media-amazon.com/images/M/MV5BM2ZhMzE0MWItYmRjZS00ODBjLTkyNDgtOGZjMzgxN2Q2NjAxXkEyXkFqcGdeQXVyMTIwMjY0NjQz._V1_.jpg', 
-            'answer': 'Stuck in the Suburbs'
+            'answer': 'Stuck in the Suburbs',
+            'category': 'Movies',
+            'subcategory': 'Disney Channel Original Movies',
+        },
+        {
+            'img_url': 'https://www.commonsensemedia.org/sites/default/files/styles/social_share_image/public/screenshots/csm-movie/jen2.jpg', 
+            'answer': 'The Jennie Project',
+            'category': 'Movies',
+            'subcategory': 'Disney Channel Original Movies',
+        },
+        {
+            'img_url': 'https://m.media-amazon.com/images/M/MV5BOTM4MTMwZWItNmZkYy00ZmE4LTkyMGYtZGRmOWQ1ZWYwZDNiXkEyXkFqcGdeQXVyODg0NTYwMjM@._V1_.jpg', 
+            'answer': 'Cadet Kelly',
+            'category': 'Movies',
+            'subcategory': 'Disney Channel Original Movies',
         },
     ]
     db.questions.insert_many(questions)
